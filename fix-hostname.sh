db153145042162c4454444232bfca20966cfecbd
TJ Luoma <luomat@gmail.com>
2015-05-16 22:27:04 -0400

    Initial Commit

---
+++ fix-hostname.sh
@@ -0,0 +1,90 @@
+#!/bin/zsh -f
+############################################################################################################
+# From:	Tj Luo.ma
+# Mail:	luomat at gmail dot com
+# Web: 	http://RhymesWithDiploma.com
+# Date:	2015-05-16
+#
+# Purpose: OS X 10.10 has a problem where the 'LocalHostName' ends up with a '-2' at the end of the name.
+#			To work around this, I set the 'ComputerName' to the same thing as what I want LocalHostName to
+#			be, and then use this script to make sure they are identical.
+#
+# If you want this script to be able to run unattended (i.e. via `launchd`), then you need to use
+# 'sudo visudo' to add one of these lines to your sudoers file:
+#
+# 	%admin ALL=NOPASSWD: /usr/sbin/scutil
+#
+# or
+#
+# 	%admin ALL=NOPASSWD: /usr/sbin/scutil --set LocalHostName
+#
+# If you are the only user of your Mac, I recommend  first one.
+#
+# You can check to see what your ComputerName and LocalHostName values are using:
+#
+#	scutil --get ComputerName
+#
+# and
+#
+# 	scutil --get LocalHostName
+#
+# For more info on these names, see:
+# http://ilostmynotes.blogspot.com/2012/03/computername-vs-localhostname-vs.html
+#
+############################################################################################################
+
+NAME="$0:t:r"
+
+LOCAL_HOST_NAME=`/usr/sbin/scutil --get LocalHostName`
+
+COMPUTER_NAME=`/usr/sbin/scutil --get ComputerName`
+
+	# If the values are identical, quit (nothing to fix)
+[[ "$COMPUTER_NAME" == "$LOCAL_HOST_NAME" ]] && exit 0
+
+####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
+#
+#		If we get to this point, the hostnames do not match, but should
+#
+zmodload zsh/datetime
+
+function msg {
+
+
+	function timestamp { strftime "%Y-%m-%d--%H.%M.%S" "$EPOCHSECONDS" }
+
+	echo "$NAME [`timestamp`]: $@" | tee -a "$HOME/Library/Logs/$NAME.log"
+
+	if (( $+commands[po.sh] ))
+	then
+		po.sh "$@"
+	fi
+}
+
+	# Make sure that 'ComputerName' is actually set to something
+if [[ "$COMPUTER_NAME" == "" ]]
+then
+	msg "\$COMPUTER_NAME is empty. Use 'sudo scutil --set ComputerName XYZ' to set it to XYZ"
+	exit 0
+fi
+
+	# Try to set the LocalHostName to whatever the ComputerName is:
+sudo /usr/sbin/scutil --set LocalHostName "$COMPUTER_NAME"
+
+SET_LOCAL_HOST_NAME="$?"
+
+	# Get the new value of LocalHostName (to see if it was successful)
+LOCAL_HOST_NAME=`/usr/sbin/scutil --get LocalHostName`
+
+if [[ "$COMPUTER_NAME" == "$LOCAL_HOST_NAME" ]]
+then
+		# It was successful
+	msg "Successfully reset LocalHostName to $LOCAL_HOST_NAME"
+else
+		# It was not successful
+	msg "Failed to reset LocalHostName ($LOCAL_HOST_NAME) to ComputerName ($COMPUTER_NAME). scutil exit code: $SET_LOCAL_HOST_NAME (should be zero)"
+fi
+
+exit 0
+#
+#EOF
